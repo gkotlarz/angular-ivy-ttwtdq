fe131028a5c3a1298fd93625876c3bb4ee3b4215
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-02-21 02:50:24 +0100

    fixing missing products Product with minimal changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import { Products, Product } from './products';
+import { products, Product } from './products';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,9 +1,13 @@
-
-
-
 export interface Product {
   name: string;
   description: string;
-  price: bigint;
+  price: number;
 }
 
+var products_tmp = [
+  {name: 'aa', description: 'aa aa', price: 1},
+  {name: 'bb', description: 'bb bb', price: 2},
+  {name: 'cc', description: 'cc cc', price: 3},
+]
+
+export const products: Product[] = products_tmp
