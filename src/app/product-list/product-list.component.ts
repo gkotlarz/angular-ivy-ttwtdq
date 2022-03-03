55510da12a4b66b48eaeea2c79c7dead9e0c2e18
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-03-03 01:52:44 +0100

    Cleaning component - templates.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,22 +7,21 @@ import { Product, products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  @Input() products = products;
-  @Input() products2: Product[] = products;
+  @Input() products: Product[] = products;
 
   constructor() {
-    console.log(this.products2);
+    console.log(this.products);
   }
 
   ngOnInit() {
-    console.log(this.products2);
+    console.log(this.products);
   }
 
   share() {
-    console.log(this.products2);
-    console.log(this.products2.length);
-    console.log(this.products2[0].name);
-    console.log(this.products2[0].description);
+    console.log(this.products);
+    console.log(this.products.length);
+    console.log(this.products[0].name);
+    console.log(this.products[0].description);
     //window.alert('share');
     console.log('manual update');
   }
