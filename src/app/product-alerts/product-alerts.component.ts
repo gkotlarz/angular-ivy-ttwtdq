e9368fd238b5bce71de4741716564a5d7dc38ee9
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-03-03 02:15:26 +0100

    Displaying componen property in {{interpolation}}

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { products, Product } from '../products';
 
-//decorator @Component
+// decorator @Component
 // The @Component() decorator indicates that the following class is a component
+// metadata about the component, including its selector, templates, and styles.
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
-  //The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
+  // The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
   @Input()
   product!: Product;
 
