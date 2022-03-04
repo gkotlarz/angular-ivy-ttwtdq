4e1ed6567afc2b8ac4fd3dc9040a3f69138b6937
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-03-04 23:29:30 +0100

    Injecting ActivatedRoute in component constructor. ngOnInit uses injected route: ActivatedRoute to set component's product field. product-details html template displays data from product.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product, products } from '../products';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  // From StackOverflow both declartions below are equal. 
+  //   This let variable be undefinied after constructor call, and lets this variable be injected by CDI
+  // product!: Product;
+  product: Product | undefined;
 
-  constructor() {
+  // ActivatedRoute is specific to each component that the Angular Router loads. ActivatedRoute contains information about the route and the route's parameters.
+  // By injecting ActivatedRoute, you are configuring the component to use a service. The Managing Data step covers services in more detail.
+  // By injecting ActivatedRoute, you are configuring the component to use a service. 
+  constructor(private route: ActivatedRoute) {
     // BTW. each time routing routes to component, 
     // constructor is called  - means new instance is created!
     console.log("ProductDetailsComponent constructor. ");
   }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute); 
+
+    console.log("ProductDetailsComponent ngOnInit. ");
   }
 
 }
