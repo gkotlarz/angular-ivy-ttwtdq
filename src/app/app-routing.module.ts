b39d3bbea98c19cc3f3a5d03e90391c66af043f3
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-03-04 02:45:34 +0100

    Cleaning, clarifing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 const routes: Routes = [
   { path: "", component: ProductListComponent },
   { path: "products/:productId", component: ProductDetailsComponent },
-  { path: "productdetails", component: HelloComponent}
+  { path: "hello", component: HelloComponent}
 ];
 
 @NgModule({
