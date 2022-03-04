c288d2884a837e0a5dc7196d0c5f579283065755
Grzegorz Kotlarz <grzegorz.kotlarz@iapps-technologies.com>
2022-03-04 02:48:55 +0100

    Improving data temp data for display.

--- src/app/products.ts
+++ src/app/products.ts
@@ -6,9 +6,9 @@ export interface Product {
 }
 
 var products_tmp = [
-  {id: 1, name: 'aa', description: 'aa aa', price: 1},
-  {id: 2, name: 'bb', description: 'bb bb', price: 2000},
-  {id: 3, name: 'cc', description: '', price: 3},
+  {id: 1, name: 'Audi A4', description: 'The oldest one.', price: 1},
+  {id: 2, name: 'Volkswagen Passat', description: 'Big combi.', price: 2000},
+  {id: 3, name: 'Fiat Tipo2', description: '', price: 3},
 ]
 
 export const products: Product[] = products_tmp
