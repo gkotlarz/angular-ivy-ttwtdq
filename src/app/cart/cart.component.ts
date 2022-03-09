c8af5af9ea8f5b305ec965d16007f83d63083667
Grzegorz Kotlarz <gkotlarz@gmail.com>
2022-03-10 00:59:00 +0100

    Service cartService is injected to cartComponent in contstructr.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  items = this.cartService.getItems();
+
+  // injecting Service with constructor.
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
