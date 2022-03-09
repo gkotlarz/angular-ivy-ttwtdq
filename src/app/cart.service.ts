c5c58934cff4902e283ca83f98572b6aff54d52c
Grzegorz Kotlarz <gkotlarz@gmail.com>
2022-03-10 00:15:56 +0100

    Adding methods to service (@Injectable): cart.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products'
 
+/** In Angular, a service is an instance of a class that you can make available to any part of your application 
+ * using Angular's dependency injection system.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  items : Product[] = [];
   constructor() { }
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
